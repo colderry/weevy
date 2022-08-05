30245172ff0cf71814c3931b0a6128c43c6f5b0f
colderry <c0l4e6r7@gmail.com>
2022-08-06 00:05:35 +0600

    Add path at the top

--- bin/weevy.ts
+++ bin/weevy.ts
@@ -1,4 +1,5 @@
-#! /usr/bin/env node
+#!/usr/bin/env node
+
 import helpCLI from "../cli/help-cli";
 import startCLI from "../cli/start-cli";
 
--- cli/help-cli.ts
+++ cli/help-cli.ts
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 function logCommand (name: string, desc: string) {
 	console.log(`    ${name}`, `=> ${desc}`);
 }
